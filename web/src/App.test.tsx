9746df51b5bb70a6d419b666cb62e89f2c2354b9
abualif1413 <ilhamakbar_90@hotmail.com>
2022-03-28 13:19:52 +0700

    Add unit test for the application

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { ProductStatus, ProductList } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Delivery status must be the same with disabled button', () => {
+  const { getByTestId } = render(
+    <ProductList
+      productId={1}
+      productName='Product name'
+      deliveryStatus='ORDERED'
+      productType='Desert'
+      orderedBy='Ordered By'
+      estimatedDeliveryDate='Some Date'
+      onButtonStatusClicked={jest.fn()}
+    />
+  );
+  const deliveryStatusTest = getByTestId("delivery-status-1").textContent;
+  const deliveryButtonTest = (getByTestId(`delivery-button-${deliveryStatusTest}-1`) as HTMLButtonElement).disabled;
+  expect(deliveryButtonTest).toBe(true);
+});
+
+test('At least one and only one delivery status button is disabled to determine the delivery status', () => {
+  const { getByTestId } = render(
+    <ProductList
+      productId={1}
+      productName='Product name'
+      deliveryStatus='ORDERED'
+      productType='Desert'
+      orderedBy='Ordered By'
+      estimatedDeliveryDate='Some Date'
+      onButtonStatusClicked={jest.fn()}
+    />
+  );
+  const allDeliveryStatus = [ProductStatus.CANCELLED, ProductStatus.ORDERED, ProductStatus.PENDING, ProductStatus.SHIPPED];
+  let noOfDisabled = 0;
+  allDeliveryStatus.forEach(status => {
+    const deliveryButtonTest = (getByTestId(`delivery-button-${status}-1`) as HTMLButtonElement).disabled;
+    if(deliveryButtonTest === true) {
+      noOfDisabled++;
+    }
+  })
+  
+  expect(noOfDisabled).toBe(1);
 });
